2e7d566feb956e31b645047570d97a7a08599208
Ercan Öztürk <ercanozturk788@gmail.com>
2024-11-08 21:25:33 +0300

    Contact and resume info

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,20 @@ import { RouterLink, RouterView } from "vue-router";
 
 <template>
   <div class="flex items-center justify-center h-screen">
-    <div class="rows-2 items-center mx-10">
+    <div class="rows-3 items-center mx-10">
       <img alt=" Ercan Ozturk" class="image rounded-xl center p-2" src="@/assets/ercanozturk.jpg" width="500"
         height="500" />
       <div class="mx-2 rounded bg-green-800">
-        <div class="font-bold text-xl mb-2 text-center">Ercan Öztürk</div>
+        <div class="font-mono text-xl mb-2 text-center">Ercan Öztürk</div>
+      </div>
+      <div class="flex justify-center items-center">
+        <i class="mx-4 pi pi-envelope" style="color: green"></i>
+        <h5 class="text-white text-base font-mono">ercanozturk222@gmail.com</h5>
+      </div>
+      <div class="flex justify-center items-center">
+        <a href="https://github.com/Ercan-Ozturk">
+          <img src="./assets/github_logo.png" height="72" width="72">
+        </a>
       </div>
     </div>
     <div>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,36 +6,41 @@
     </div>
 
     <div class="grid grid-rows-5 grid-flow-col gap-5 px-6 py-2">
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">
-        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">
+        <div>Ability to work in the EU (Bulgaria and
+          Turkey Citizenship)</div>
       </div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit. Nunc a purus</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">viverra, placerat ante vitae, aliquet neque.
-        Vestibulum vestibulum tellus</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">non ultrices sagittis. Nullam commodo risus
-        consequat leo efficitur</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">mattis. Pellentesque a ligula eget est rutrum
-        posuere ut eget lacus. Cras</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">placerat velit eget faucibus mollis. Vivamus luctus
-        nibh sit amet est</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">dignissim facilisis quis at magna. Vestibulum
-        efficitur arcu nec auctor</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">tincidunt. Vestibulum consectetur metus varius,
-        consequat leo id, commodo</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">leo. Duis gravida justo nec vehicula aliquam. Nulla
-        fringilla justo eget</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">diam posuere molestie. Vivamus eu lectus aliquet,
-        dictum leo nec, luctus</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">ante. Nullam eu purus quis mi vulputate interdum.
-        Donec vestibulum nisi</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">nec sollicitudin consequat. Vivamus commodo lorem
-        vel dui condimentum</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">pulvinar. Etiam sit amet massa eu lorem ornare
-        finibus. Quisque sed tortor</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">ac ex fringilla interdum a a mauris. Mauris
-        hendrerit turpis vitae nisi</div>
-      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">rhoncus vestibulum.</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Highly skilled with Kotlin, Jetpack
+        Compose
+        and XML</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Multi-platform application experience
+        with Flutter, KMM and React Native</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Experience with navigation on Android
+        and iOS</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Highly skilled with Swift, SwiftUI,
+        UIKit and
+        AutoLayout</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Experience on databases such as MySQL,
+        SQL,
+        SQLite, PostgreSQL, and MongoDB</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Experience programming with Rust</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Experience working with Vue.js, Nuxt
+      </div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Worked on API’s with Node.js, Express
+      </div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Extensive knowledge on C# on Unity
+        and .NET as well as C and C++</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Highly proficient with Python</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Full Stack app making
+        experience with Django and
+        Flask</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Experience on databases such as MySQL,
+        SQL,
+        SQLite, PostgreSQL, and MongoDB</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">DL - Tensorflow/Keras/PyTorch</div>
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative text-center">Turkish – Native English – Professional
+        Dutch -
+        Conversational.</div>
     </div>
   </div>
 </template>
