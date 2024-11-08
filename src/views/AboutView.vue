b90bd50ae20e3313906e784c612c82a4c582f3f1
Ercan Öztürk <ercanozturk788@gmail.com>
2024-11-08 17:26:32 +0300

    Removing templates

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="bg-white rounded-xl shadow-md relative">
-    <div class="px-6 py-4">
+  <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+    <div>Vue.js</div>
+    <div>Vue.js</div>
+
+    <div>Vue.js</div>
+    <div>Vue.js</div>
+  </div>
+  <div class="bg-black rounded-xl shadow-md relative">
+    <div class="px-6 py-4 rounded bg-black">
       <div class="font-bold text-xl mb-2">Ercan Öztürk</div>
     </div>
-    <div class="px-6 pt-4 pb-2">
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#photography</span
-      >
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#travel</span
-      >
-      <span
-        class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-        >#winter</span
-      >
+    <div class="px-6 py-2">
+      <div class="font-bold text-l">Software Engineer</div>
+    </div>
+
+    <div class="px-6 py-2 grid grid-cols-4 gap-4">
+      <div>Vue.js</div>
+      <div>Vue.js</div>
+      <div>Vue.js</div>
+      <div>Vue.js</div>
+      <div>Vue.js</div>
     </div>
   </div>
 </template>
