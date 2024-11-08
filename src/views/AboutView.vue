297232b976ce2fc9deae97fc51c582050c65b765
Ercan Öztürk <ercanozturk788@gmail.com>
2024-11-08 17:50:47 +0300

    Refactoring and tidying

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,5 @@
 <template>
-  <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-    <div>Vue.js</div>
-    <div>Vue.js</div>
-
-    <div>Vue.js</div>
-    <div>Vue.js</div>
-  </div>
-  <div class="bg-black rounded-xl shadow-md relative">
+  <div class="bg-gray rounded-xl shadow-md relative">
     <div class="px-6 py-4 rounded bg-black">
       <div class="font-bold text-xl mb-2">Ercan Öztürk</div>
     </div>
@@ -14,22 +7,24 @@
       <div class="font-bold text-l">Software Engineer</div>
     </div>
 
-    <div class="px-6 py-2 grid grid-cols-4 gap-4">
-      <div>Vue.js</div>
-      <div>Vue.js</div>
-      <div>Vue.js</div>
-      <div>Vue.js</div>
-      <div>Vue.js</div>
+    <div class="grid grid-flow-col gap-4 auto-cols-auto px-6 py-2">
+      <div class="px-2 py-2 bg-black rounded-xl shadow-md relative">
+        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus</div>
+      </div>
+      <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc a purus</div>
+      <div>viverra, placerat ante vitae, aliquet neque. Vestibulum vestibulum tellus</div>
+      <div>non ultrices sagittis. Nullam commodo risus consequat leo efficitur</div>
+      <div>mattis. Pellentesque a ligula eget est rutrum posuere ut eget lacus. Cras</div>
+      <div>placerat velit eget faucibus mollis. Vivamus luctus nibh sit amet est</div>
+      <div>dignissim facilisis quis at magna. Vestibulum efficitur arcu nec auctor</div>
+      <div>tincidunt. Vestibulum consectetur metus varius, consequat leo id, commodo</div>
+      <div>leo. Duis gravida justo nec vehicula aliquam. Nulla fringilla justo eget</div>
+      <div>diam posuere molestie. Vivamus eu lectus aliquet, dictum leo nec, luctus</div>
+      <div>ante. Nullam eu purus quis mi vulputate interdum. Donec vestibulum nisi</div>
+      <div>nec sollicitudin consequat. Vivamus commodo lorem vel dui condimentum</div>
+      <div>pulvinar. Etiam sit amet massa eu lorem ornare finibus. Quisque sed tortor</div>
+      <div>ac ex fringilla interdum a a mauris. Mauris hendrerit turpis vitae nisi</div>
+      <div>rhoncus vestibulum.</div>
     </div>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
