3157919b84d123316acce0cded4469f0e4660f07
Ercan Öztürk <ercanozturk788@gmail.com>
2024-11-14 22:04:30 +0300

    Tailwind overflow fix and format

--- src/components/InfoCard.vue
+++ src/components/InfoCard.vue
@@ -1,18 +1,18 @@
 <script setup>
 defineProps({
-    bg: {
-        type: String,
-        default: "bg-gray-100",
-    },
+  bg: {
+    type: String,
+    default: "bg-gray-100",
+  },
 });
 </script>
 
 <template>
-    <div
-        class="px-2 py-2 bg-gradient-to-t from-gray-900 to-green-700 rounded-xl shadow-xl drop-shadow-xl relative text-center">
-        <div class="text-slate-50">
-            <slot> </slot>
-        </div>
-
+  <div
+    class="px-2 py-2 bg-gradient-to-t from-gray-900 to-green-700 rounded-xl shadow-xl drop-shadow-xl relative text-center"
+  >
+    <div class="text-slate-50">
+      <slot> </slot>
     </div>
+  </div>
 </template>
